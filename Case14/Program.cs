05b469f26e55adb976dcce12fd241692b81a7738
ZaurKhakurinov <lazter@mail.ru>
2022-12-14 00:27:44 +0300

    add work on the lesson

---
+++ Case14/Program.cs
@@ -0,0 +1,22 @@
+﻿// написать программу, которая на вход число и проверяет]]]6 кратно ли оно одновременно и 7 и 23. (14, 46, 161)
+
+// Console.Clear();
+// int num = int.Parse(Console. ReadLine());
+// if (num % 7 == 0 & num % 23 == 0);
+// {
+//     Console.WriteLine($"{num} -> да");
+// }
+// else
+// {
+//     Console.WriteLine($"{num} -> нет");
+// }
+
+Console.Clear();
+int num = int.Parse(Console.ReadLine());
+if ((num%7==0) && (num%23==0))
+{
+    Console.WriteLine($"{num}-> да");
+}else
+{
+    Console.WriteLine($"{num}-> нет");
+}
