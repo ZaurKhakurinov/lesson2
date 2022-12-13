8fd468fe0dea677b4191afd41d4db7b798c6e0b0
ZaurKhakurinov <lazter@mail.ru>
2022-12-13 22:47:08 +0300

    add work on the lesson

---
+++ Case11/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу которая выводит случайное трехзначное число и удаляет вторую цифру этого числа
+
+Console.Clear ();
+int num = new Random(). Next(100,1000);
+Console.WriteLine($"число {num}");
+int num1 = num/100;
+int num2 = num%10;
+Console.WriteLine($"Цифра {num1}{num2}");
