fc1f3f530dfce341c23148cb07a32d070044ff4b
ZaurKhakurinov <lazter@mail.ru>
2022-12-14 21:19:01 +0300

    add homework case 10

---
+++ Case10/Program.cs
@@ -0,0 +1,7 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+int num = new Random(). Next(100,1000);
+Console.WriteLine($"число {num}");
+int num1 = num/10;
+int num2 = num1%10;
+Console.WriteLine($"{num} -> {num2}");
